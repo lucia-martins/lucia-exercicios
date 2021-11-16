a0ed0052fd4dd2998cee8fadf0731ffceb1efd71
luciamartins <92489021+lucia-martins@users.noreply.github.com>
2021-11-15 23:54:03 -0200

    aula 15 nov

---
+++ p01ex012/script.js
@@ -0,0 +1,26 @@
+let res = document.getElementById("resultado")
+let frase = document.getElementById("frase") 
+
+
+function fechar() {
+
+    res.style.visibility = "hidden"
+
+}
+
+function calcdesconto() {
+    let prod = document.getElementById("prod").value
+    let preco = Number(document.getElementById("preco").value)
+    let desc = Number(document.getElementById("desc").value)
+
+    let reduc = preco * desc / 100
+    let novo = preco - reduc
+
+    frase.innerHTML = `O ${prod}, que custava R$${preco.toFixed(2)}, com ${desc}% de deconto vai baixar R$${reduc} e vai passar a custar R$${novo.toFixed(2)}.`
+           
+    res.style.visibility = "visible"
+    
+    return false
+
+    }
+
---
+++ p01ex014/script.js
@@ -0,0 +1,16 @@
+let res = document.getElementById("resultado")
+let frase = document.getElementById("frase")
+
+function fechar () {
+    res.style.visibility = "hidden"
+
+}
+
+function converter() {
+    let real = Number(document.getElementById("real").value)
+    let dolar = real / 5.59
+    frase.innerHTML = `R$${real.toFixed(2)} corresponde a U$${dolar.toFixed(2)}`
+    res.style.visibility = "visible"
+    return false
+    
+}
